86aa7ff4463d3182e649a59875efc820f5405c7a
hekker-man <github.award390@passmail.net>
2025-02-25 21:03:33 +0100

    Imported malilib

--- src/main/java/com/hekker/firesight/FiresightClient.java
+++ src/main/java/com/hekker/firesight/FiresightClient.java
@@ -2,13 +2,18 @@ package com.hekker.firesight;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.minecraft.util.math.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FiresightClient implements ClientModInitializer {
+    public static final String MOD_ID = "firesight";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
-        Firesight.LOGGER.info("Loading Firesight client");
+        LOGGER.info("Loading Firesight");
+        BlockPos pos = new BlockPos(0, 70, 0);
+        LOGGER.info(pos.toString());
     }
 }
